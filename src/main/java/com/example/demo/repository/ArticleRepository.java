b36f535aaa9cb7e373040205d562d78e69c8502b
sak003 <sakshikuthe336@gmail.com>
2026-02-22 23:11:44 +0530

    Done Creat article endpoint and Admin Approval endpoint

---
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Article;
+import com.example.demo.model.ArticleStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    List<Article> findByStatus(ArticleStatus status);
+}
